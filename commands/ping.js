38dcf325e3518df9bc7dc6705e921e927e1a8fc3
ikifar2012 <ikifar2012@users.noreply.github.com>
2022-07-05 01:03:16 +0000

    Update ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		return interaction.reply('Pong!');
+		return interaction.reply({ content: `Pong ${global.client.ws.ping}ms`, ephemeral: true });
 	},
 };
 
