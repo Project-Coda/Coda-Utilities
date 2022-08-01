671b986d25cd81f17a96e65147c50c0865d9a7cf
ikifar2012 <ikifar2012@users.noreply.github.com>
2022-08-01 03:25:27 +0000

    added youtube command might move to seprate bot

---
+++ commands/youtube.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const play = require('../utilities/play.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('youtube')
+		.setDescription('Allows you to play music from YouTube.')
+		.addStringOption(option =>
+			option.setName('url')
+				.setDescription('The URL of the YouTube video to play.')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('volume')
+				.setDescription('The volume of the song.')
+				.setRequired(false)),
+	async execute(interaction) {
+		// check if user is in a voice channel
+		if (!interaction.member.voice.channel) {
+			interaction.reply('You must be in a voice channel to use this command.');
+			return;
+		}
+		try {
+			// get voice channel
+			const voiceChannel = interaction.member.voice.channel;
+			// get url
+			const url = interaction.options.get('url').value;
+			// get volume
+			// if (interaction.options.get('volume').value) {
+			// 	volume = interaction.options.get('volume').value;
+			// }
+			volume = 1;
+			// play song
+			channel = await play.joinVC(voiceChannel);
+			player = await play.createPlayer();
+			response = await play.YouTube(player, url, volume, channel);
+			embed = await play.createEmbed(response);
+			return interaction.reply({ embeds:[embed], ephemeral: true });
+		}
+		catch (error) {
+			console.log(error);
+			return interaction.reply({ content: error, ephemeral: true });
+		}
+	},
+
+};
---
+++ utilities/play.js
@@ -0,0 +1,71 @@
+const { createAudioPlayer, NoSubscriberBehavior, createAudioResource, joinVoiceChannel } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const embedcreator = require('../embed.js');
+
+// Create Track Class
+class Track {
+	constructor(name, url, artist, image) {
+		this.name = name;
+		this.url = url;
+		this.artist = artist;
+		this.image = image;
+	}
+}
+async function createEmbed(track) {
+	embed = embedcreator.setembed ({
+		title: `Now Playing: ${track.name}`,
+		url: track.url,
+		description: `**Artist:** ${track.artist}\n**URL:** ${track.url}`,
+		color: 0x19ebfe,
+		image: {
+			url: track.image,
+		},
+	});
+	return embed;
+}
+async function joinVC(channel) {
+	channel = joinVoiceChannel({
+		channelId: channel.id,
+		guildId: channel.guild.id,
+		adapterCreator: channel.guild.voiceAdapterCreator,
+	});
+	return channel;
+}
+async function YouTube(player, url, volume, channel) {
+	try {
+		info = await ytdl.getBasicInfo(url);
+		const yt = ytdl(url, {
+			filter: 'audioonly',
+		});
+		const resource = createAudioResource(yt, {
+			inlineVolume: true,
+		});
+		await player.play(resource);
+		await channel.subscribe(player);
+		if (volume) {
+			resource.volume.setVolume(volume);
+		}
+		console.log(info.videoDetails.thumbnails);
+		track = new Track(info.videoDetails.title, url, info.videoDetails.author.name, info.videoDetails.thumbnails[4].url);
+		return track;
+	}
+	catch (error) {
+		console.log(error);
+		return error;
+	}
+}
+async function createPlayer() {
+	const player = createAudioPlayer(
+		{
+			noSubscriberBehavior: NoSubscriberBehavior.Stop,
+			noSubscriberBehaviorTimeout: 10000,
+		},
+	);
+	return player;
+}
+module.exports = {
+	YouTube,
+	createPlayer,
+	joinVC,
+	createEmbed,
+};
