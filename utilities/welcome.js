7032fba9a88cf4dac33f1eeee0432db8873de2f3
ikifar2012 <ikifar2012@users.noreply.github.com>
2022-11-22 03:03:50 +0000

    cleanup welcome

--- utilities/welcome.js
+++ utilities/welcome.js
@@ -3,14 +3,14 @@ const fetch = require('node-fetch');
 async function getList() {
 	file = await fetch(env.utilities.welcomemessagesfile);
 	messages = await file.text();
-	messages = messages.split('end');
+	messages = messages.split(' end');
 	return messages;
 }
 
 async function getWelcome() {
 	const welcomelist = await getList();
-	const welcome = welcomelist[Math.floor(Math.random() * welcomelist.length)];
-	return welcome;
+	const welcome = welcomelist[Math.floor(Math.random() * (welcomelist.length - 1))];
+	return welcome.replace(/\\n/g, '\n');
 }
 
 module.exports = { getList, getWelcome };
